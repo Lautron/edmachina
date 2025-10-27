5d847de7d54f4543d77ec7197324b543493dcd45
Lautaro Bachmann <laurigbach@gmail.com>
2025-10-27 20:28:59 -0300

    Add sidebar provider to App component

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,10 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
+import { SidebarProvider } from '@/components/ui/sidebar'
 </script>
 
 <template>
-  <header>
-    <div class="wrapper my-10">
-    <nav>
-      <RouterLink to="/login" class="px-4 py-2 border border-gray-300 rounded mr-2">Login</RouterLink>
-      <RouterLink to="/register" class="px-4 py-2 border border-gray-300 rounded mr-2">Register</RouterLink>
-      <RouterLink to="/forgot-password" class="px-4 py-2 border border-gray-300 rounded mr-2">Forgot Password</RouterLink>
-      <RouterLink to="/" class="px-4 py-2 border border-gray-300 rounded">Dashboard</RouterLink>
-    </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <SidebarProvider>
+    <RouterView />
+  </SidebarProvider>
 </template>
