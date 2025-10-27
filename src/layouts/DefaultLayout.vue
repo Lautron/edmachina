c2a45295096fdef15216d8424c81b3b03ea3a3ec
Lautaro Bachmann <laurigbach@gmail.com>
2025-10-27 20:28:04 -0300

    Create default layout

---
+++ src/layouts/DefaultLayout.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import TheSidebar from '@/components/TheSidebar.vue'
+</script>
+
+<template>
+  <div class="flex h-screen">
+    <TheSidebar />
+    <div class="flex flex-col flex-1">
+      <main class="flex-1 overflow-y-auto p-4">
+        <router-view />
+      </main>
+    </div>
+  </div>
+</template>
