0f6999145e2b613cb93551e5c7827b3e2e3bdbd1
Lautaro Bachmann <laurigbach@gmail.com>
2025-10-27 21:02:34 -0300

    Add cat icon to sidebar

--- src/components/TheSidebar.vue
+++ src/components/TheSidebar.vue
@@ -1,18 +1,22 @@
 <script setup lang="ts">
+import { Cat } from 'lucide-vue-next'
 import {
   Sidebar,
   SidebarContent,
   SidebarHeader,
   SidebarMenu,
   SidebarMenuItem,
-  SidebarMenuButton,
+  SidebarMenuButton
 } from '@/components/ui/sidebar'
 </script>
 
 <template>
-  <Sidebar class="h-screen">
+  <Sidebar>
     <SidebarHeader>
-      <h2 class="text-2xl font-bold">PetCare</h2>
+      <div class="flex items-center gap-2 p-2">
+         <Cat class="size-7 text-primary" />
+        <h2 class="text-xl font-bold">PetCare.</h2>
+      </div>
     </SidebarHeader>
     <SidebarContent>
       <SidebarMenu>
@@ -23,4 +27,3 @@ import {
     </SidebarContent>
   </Sidebar>
 </template>
-
