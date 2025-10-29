727a6e83979d942374defe0188ef327770e0189b
Lautaro Bachmann <laurigbach@gmail.com>
2025-10-28 21:04:16 -0300

    Create DefaultView for empty pages on the sidebar

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,35 +2,70 @@ import { createRouter, createWebHistory } from 'vue-router';
 import DefaultLayout from '@/layouts/DefaultLayout.vue';
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/login',
-      name: 'login',
-      component: () => import('@/views/LoginView.vue')
-    },
-    {
-      path: '/register',
-      name: 'register',
-      component: () => import('@/views/RegisterView.vue')
-    },
-    {
-      path: '/forgot-password',
-      name: 'forgot-password',
-      component: () => import('@/views/ForgotPasswordView.vue')
-    },
-    {
-      path: '/',
-      component: DefaultLayout,
-      children: [
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: [
         {
-          path: '',
-          name: 'dashboard',
-          component: () => import('@/views/DashboardView.vue')
+            path: '/login',
+            name: 'login',
+            component: () => import('@/views/LoginView.vue')
+        },
+        {
+            path: '/register',
+            name: 'register',
+            component: () => import('@/views/RegisterView.vue')
+        },
+        {
+            path: '/forgot-password',
+            name: 'forgot-password',
+            component: () => import('@/views/ForgotPasswordView.vue')
+        },
+        {
+            path: '/',
+            component: DefaultLayout,
+            children: [
+                {
+                    path: '',
+                    name: 'dashboard',
+                    component: () => import('@/views/DashboardView.vue')
+                },
+                {
+                    path: '/pet-profile',
+                    name: 'pet-profile-default',
+                    component: () => import('@/views/DefaultView.vue')
+                },
+                {
+                    path: '/health',
+                    name: 'health-default',
+                    component: () => import('@/views/DefaultView.vue')
+                },
+                {
+                    path: '/vaccination',
+                    name: 'vaccination-default',
+                    component: () => import('@/views/DefaultView.vue')
+                },
+                {
+                    path: '/chat',
+                    name: 'chat-default',
+                    component: () => import('@/views/DefaultView.vue')
+                },
+                {
+                    path: '/appointments',
+                    name: 'appointments-default',
+                    component: () => import('@/views/DefaultView.vue')
+                },
+                {
+                    path: '/settings',
+                    name: 'settings-default',
+                    component: () => import('@/views/DefaultView.vue')
+                },
+                {
+                    path: '/documentation',
+                    name: 'documentation-default',
+                    component: () => import('@/views/DefaultView.vue')
+                },
+            ]
         }
-      ]
-    }
-  ]
+    ]
 });
 
 export default router;
---
+++ src/views/DefaultView.vue
@@ -0,0 +1,17 @@
+<script setup lang='ts'>
+import {computed} from 'vue'
+import {useRoute} from 'vue-router'
+const route = useRoute()
+const formattedTitle = computed( () => {
+    let str = route.path.replace(/^\/|\/$/g, "")
+    str = str.replace(/-/g, " ")
+    str = str.replace(/\b\w/g, c => c.toUpperCase())
+    return str
+})
+</script>
+
+<template>
+  <div>
+      <h1>{{formattedTitle}} Page</h1>
+    </div>
+</template>
