01df7a974a165024b5a335069b9b80e79019ff44
Lautaro Bachmann <laurigbach@gmail.com>
2025-10-30 16:28:41 -0300

    Modify project config to prevent build error

---
+++ env.d.ts
@@ -0,0 +1,42 @@
+// <reference types="vite/client" />
+
+declare module '*.vue' {
+  import type { DefineComponent } from 'vue'
+  const component: DefineComponent<{}, {}, any>
+  export default component
+}
+
+// Handle static asset imports (images, svgs, etc.)
+// This tells TypeScript that importing these files
+// will return a string (the asset's URL).
+
+declare module '*.jpg' {
+  const src: string;
+  export default src;
+}
+
+declare module '*.jpeg' {
+  const src: string;
+  export default src;
+}
+
+declare module '*.png' {
+  const src: string;
+  export default src;
+}
+
+declare module '*.svg' {
+  const src: string;
+  export default src;
+}
+
+declare module '*.gif' {
+  const src: string;
+  export default src;
+}
+
+declare module '*.bmp' {
+  const src: string;
+  export default src;
+}
+
